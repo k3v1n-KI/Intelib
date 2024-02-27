21ebddef8da6cc9a227debd011c363bbe63eeb51
Kevin Igweh <kevinigwehik20@gmail.com>
2024-02-27 18:39:55 -0500

    Converted Figma designs to html files and integrated the files into flask

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
