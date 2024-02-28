8534c26580590e033e4ed9ad4761a6691b8098bc
Kevin Igweh <kevinigwehik20@gmail.com>
2024-02-27 22:10:44 -0500

    Initialized the mongoDB client and created the register user page

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+
 
 app = Flask(__name__)
+client = MongoClient("localhost", 27017)
+
+db = client.flask_database
+users = db.users
+
 
 @app.route("/")
 def home():
@@ -10,6 +18,10 @@ def home():
 def login():
     return render_template("login.html")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
