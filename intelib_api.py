4040595896dc1d09d7a863d096fe7ab9a4a2c5be
Kevin Igweh <kevinigwehik20@gmail.com>
2024-02-29 12:56:56 -0500

    Implemented Update user feature

--- intelib_api.py
+++ intelib_api.py
@@ -1,6 +1,6 @@
 from flask import Flask, abort, session
 from flask_restful import Resource, Api, reqparse
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 from flask_bcrypt import Bcrypt
 import json
 
@@ -22,6 +22,19 @@ register_args.add_argument("about_your_family", type=str, help="User's family de
 register_args.add_argument("about_your_neighborhood", type=str, help="User's neighborhood description")
 register_args.add_argument("about_your_hobbies", type=str, help="User's hobbies")
 register_args.add_argument("about_your_personality", type=str, help="User's personality description")
+
+# Arguments required to update a user's profile
+update_args = reqparse.RequestParser()
+update_args.add_argument("first_name", type=str, help="User's first name")
+update_args.add_argument("last_name", type=str, help="User's last name")
+update_args.add_argument("email", type=str, help="User's email")
+update_args.add_argument("password", type=str, help="User's password")
+update_args.add_argument("phone_number", type=int, help="User's phone number")
+update_args.add_argument("about_your_job", type=str, help="User's job description")
+update_args.add_argument("about_your_family", type=str, help="User's family description")
+update_args.add_argument("about_your_neighborhood", type=str, help="User's neighborhood description")
+update_args.add_argument("about_your_hobbies", type=str, help="User's hobbies")
+update_args.add_argument("about_your_personality", type=str, help="User's personality description")
 
 
 # Arguments requires to log a user in
@@ -75,6 +88,33 @@ class GetOneUser(Resource):
             return user
         return abort(404)
 
+# Update user endpoint
+class UpdateUser(Resource):
+    def put(self, email):
+        # Columns that are nullable
+        nullable_fields = ["about_your_job", "about_your_family", "about_your_neighborhood", 
+                           "about_your_hobbies", "about_your_personality"]
+        # Update arguments 
+        args_for_update = dict(update_args.parse_args())
+        # Removing all null attributes to only update the fields that needs to be updated.
+        # Attributes in the "nullable_fields" list can be Null
+        filtered_update_args = {key: value for key, value in args_for_update.items() \
+                                if value is not None or key in nullable_fields}
+        # If password is being updated, we hash it before sending it to the database
+        if "password" in filtered_update_args.keys():
+            hashed_password = bcrypt.generate_password_hash(filtered_update_args["password"]).decode('utf-8')
+            filtered_update_args["password"] = hashed_password
+        # Update user and return updated user
+        user = users.find_one_and_update(
+            {"email": email}, 
+            { '$set': filtered_update_args }, 
+            return_document = ReturnDocument.AFTER
+            )
+        user = dict(user)
+        del user["_id"]
+        return user
+    
+    
 # Login Endpoint
 class Login(Resource):
     def post(self):
@@ -110,6 +150,8 @@ api.add_resource(HelloWorld, "/")
 api.add_resource(Register, "/register")
 api.add_resource(GetAllUsers, "/get_all_users")
 api.add_resource(GetOneUser, "/get_one_user/<email>")
+api.add_resource(UpdateUser, "/update_user/<email>")
+api.add_resource(Login, "/login")
 api.add_resource(Logout, "/logout")
 
 if __name__ == "__main__":
