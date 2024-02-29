24428dd80d3cdc560c23955b04622011595f2402
Kevin Igweh <kevinigwehik20@gmail.com>
2024-02-29 10:53:45 -0500

    Removed google authentication

--- intelib_api.py
+++ intelib_api.py
@@ -1,15 +1,9 @@
-import os
-import pathlib
-import requests
-from flask import Flask, request, redirect, session, abort
+from flask import Flask, abort
 from flask_restful import Resource, Api, reqparse
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
 import json
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import Flow
-import google.auth.transport.requests
-from pip._vendor import cachecontrol
+
 
 # Initilize Flask API
 app = Flask("REST_API")
@@ -31,7 +25,6 @@ login_args.add_argument("password", type=str, help="User's password", required=T
 
 # MongoDB client
 client = MongoClient("localhost", 27017)
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
 
 # DB Initilization
 db = client.flask_database
@@ -39,16 +32,6 @@ users = db.users
 auth_json_file = open("intelib_google_auth.json")
 google_auth_json = json.load(auth_json_file)["web"]
 
-# Google Credentials for Sign in
-GOOGLE_CLIENT_ID = google_auth_json["client_id"]
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "intelib_google_auth.json")
-app.secret_key = google_auth_json["client_secret"]
-
-flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
-)
 
 # Base endpoint
 class HelloWorld(Resource):
@@ -109,56 +92,12 @@ class Login(Resource):
         # Return Bad request because of invalid credentials
         return abort(400)
 
-# Google login URL
-@app.route("/google_auth_login")
-def google_auth_login():
-    authorization_url, state = flow.authorization_url()
-    session["state"] = state
-    # Return user credentials that will be intercepted by the callback function
-    return redirect(authorization_url)
-
-# Google login callback
-@app.route("/callback")
-def callback():
-    # Intercepts user credentials
-    flow.fetch_token(authorization_response=request.url)
-
-    if not session["state"] == request.args["state"]:
-        abort(500)  # State does not match!
-
-    # Gets ID token that will be used to access user data
-    credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
-
-    # Verifys token for security purposes
-    id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=GOOGLE_CLIENT_ID
-    )
-    # Add the user to session and stores their credentials in the database
-    user = users.find_one({"email":id_info.get("email")})
-    if user:
-        del user["_id"]
-    else:
-        user = {
-                "first_name": id_info.get("given_name"),
-                "last_name": id_info.get("family_name"),
-                "email": id_info.get("email"),
-                "phone_number": None,
-                "password": None
-            }
-        users.insert_one(user)
-    return dict(user)
 
 # Endpoint URLs
 api.add_resource(HelloWorld, "/")
 api.add_resource(Register, "/register")
 api.add_resource(GetAllUsers, "/get_all_users")
 api.add_resource(GetOneUser, "/get_one_user/<email>")
-api.add_resource(Login, "/login")
 
 if __name__ == "__main__":
     app.run(debug=True)
